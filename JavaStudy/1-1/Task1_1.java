75f9a584ee5d64b026a8e5a02fa42782ce56b34b
ryo-s718 <74058422+ryo-s718@users.noreply.github.com>
2020-12-10 00:00:53 +0900

    提出課題
    Task1_1.java

--- JavaStudy/1-1/Task1_1.java
+++ JavaStudy/1-1/Task1_1.java
@@ -2,15 +2,14 @@ public class Task1_1 {
     public static void main(String[] args) {
         int hogeInt = 50;
         String hogeString = "テスト";
-        //
+        //［double型のhogeDoubleという名前の変数を宣言し値０．２で初期化］
         double hogeDouble = 0.2;
         hogeInt = 100;
         hogeString = "合格";
-        //
-        hogeInt = 100;
-        //
+        System.out.println(hogeInt);
+        //hogeStringの値を表示
         System.out.println(hogeString);
-        hogeDouble = 0.2;
+        System.out.println(hogeDouble);
 
     }
 }
