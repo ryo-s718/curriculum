d67194fbda85f3c08d0f876d34463d3d09d0e056
ryo-s718 <74058422+ryo-s718@users.noreply.github.com>
2020-12-09 22:13:13 +0900

    Create Task1_1.java

---
+++ JavaStudy/1-1/Task1_1.java
@@ -0,0 +1,16 @@
+public class Task1_1 {
+    public static void main(String[] args) {
+        int hogeInt = 50;
+        String hogeString = "テスト";
+        //
+        double hogeDouble = 0.2;
+        hogeInt = 100;
+        hogeString = "合格";
+        //
+        hogeInt = 100;
+        //
+        System.out.println(hogeString);
+        hogeDouble = 0.2;
+
+    }
+}
